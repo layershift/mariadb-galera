4040c4fe2ba6e12c07b163d95ce698298a2016be
Bart <contact@bartbania.com>
2015-11-11 15:02:42 +0000

    SQL

--- user.sql
+++ user.sql
@@ -1,7 +1,7 @@
-CREATE USER 'galerareplicator'@'localhost' IDENTIFIED BY PASSWORD 'GOESHERE';
-CREATE USER 'galerareplicator'@'NODE1' IDENTIFIED BY PASSWORD 'GOESHERE';
-CREATE USER 'galerareplicator'@'NODE2' IDENTIFIED BY PASSWORD 'GOESHERE';
-CREATE USER 'galerareplicator'@'NODE3' IDENTIFIED BY PASSWORD 'GOESHERE';
+CREATE USER 'galerareplicator'@'localhost' IDENTIFIED BY 'GOESHERE';
+CREATE USER 'galerareplicator'@'NODE1' IDENTIFIED BY 'GOESHERE';
+CREATE USER 'galerareplicator'@'NODE2' IDENTIFIED BY 'GOESHERE';
+CREATE USER 'galerareplicator'@'NODE3' IDENTIFIED BY 'GOESHERE';
 GRANT ALL ON *.* TO 'galerareplicator'@'localhost' with GRANT OPTION;
 GRANT ALL ON *.* TO 'galerareplicator'@'NODE1' with GRANT OPTION;
 GRANT ALL ON *.* TO 'galerareplicator'@'NODE2' with GRANT OPTION;
